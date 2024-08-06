9fe25e769f865cbf29e8433ba2761ee9cc84e734
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-08-06 13:09:21 +0500

    Initial Commit

---
+++ FoodMaster/FoodMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMasterApp.swift
+//  FoodMaster
+//
+//  Created by Erkebulan Massainov on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
